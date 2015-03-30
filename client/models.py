e9007913170a77db07d072cc4d35092d32239350
swdg-aansari <me.gauss.euler@gmail.com>
2015-03-31 01:24:00 +0430

    kandooo

---
+++ client/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+from dastband.models import FriendshipBracelet
+
+
+class Client(models.Model):
+    name = models.CharField(max_length=20)
+    user = models.OneToOneField(User)
+    bracelets = models.ManyToManyField(FriendshipBracelet)
